be24145a2265b4282743e6c5ed8c4fb6ca746c30
DESKTOP-5EP1M0T\malte <malte.gruntzdorff@gmail.com>
2017-05-28 19:22:34 +0200

    paho tests

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
+/*
+*Ausklappbare divs
+*/
 function div_hide_show(id){
 	var actualDiv = document.getElementById(id);
 	
 	if(actualDiv.style.display == "none"){
 		actualDiv.style.display = "block";
 	}
-	else{
+	else if(actualDiv.style.display == "block"){
 		actualDiv.style.display = "none";
+		
+	}
+	else {
+		actualDiv.style.display = "none"
+		div_hide_show(id);
 	}
 }
 
